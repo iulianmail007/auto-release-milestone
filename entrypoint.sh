a4843ff7f07e84673fe5c2979e5f1a50251c4ddb
Puscasu Marin <pm@pm.com>
2020-04-19 16:27:28 +0200

    blah9

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
-
 set -u
 
 repo_token=$1
 
-if ["$GITHUB_EVENT_NAME" != "milestone"]; then
-
-    echo "::debug::The event name was '$GITHUB_EVENT_NAME'"
-    exit 0
-
+if [ "$GITHUB_EVENT_NAME" != "milestone" ]; then
+  echo "::debug::The event name was '$GITHUB_EVENT_NAME'"
+  exit 0
 fi
 
 event_type=$(jq --raw-output .action $GITHUB_EVENT_PATH)
 
-if [$event_type != "closed"]; then
-    echo "::debug::The event type was '$event_type'"
-    exit 0
+if [ $event_type != "closed" ]; then
+  echo "::debug::The event type was '$event_type'"
+  exit 0
 fi
 
 milestone_name=$(jq --raw-output .milestone.title $GITHUB_EVENT_PATH)
 
+# Splits the string in $GITHUB_REPOSITORY which takes
+# the form "owner/repository" into two variables
+# called $owner and $repository
 IFS='/' read owner repository <<< "$GITHUB_REPOSITORY"
 
 release_url=$(dotnet gitreleasemanager create \
@@ -30,9 +30,9 @@ release_url=$(dotnet gitreleasemanager create \
 --owner $owner \
 --repository $repository)
 
-if [$? -ne 0];then
-    echo "::echo::Failed to create release draft"
-    exit 1
+if [ $? -ne 0 ]; then
+  echo "::error::Failed to create the release draft"
+  exit 1
 fi
 
 echo "::set-output name=release-url::$release_url"
